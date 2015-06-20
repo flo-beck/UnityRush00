e48f1ac74b9b17adf619aa1f049a2ef128c4c813
fbeck <fbeck@student.42.fr>
2015-06-20 12:54:06 +0200

    first commit with sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//face mouse
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		Debug.Log ("MOUSE POS: " + mousePos);
+		
+		//Vector3 newVec = transform.position - mousePos;
+		mousePos.z = 0;
+//		transform.LookAt(mousePos);
+		transform.LookAt(mousePos);
+
+
+
+	}
+}
