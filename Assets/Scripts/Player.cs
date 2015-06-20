0c43e52c9c84617fb9360a7c7552a59c47bd0ad3
fbeck <fbeck@student.42.fr>
2015-06-20 13:03:48 +0200

    pointing character towards camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,31 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float thrust;
+	public Rigidbody rb;
+
+	void Awake(){
+
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		//face mouse
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - mouse);
 
-		Debug.Log ("MOUSE POS: " + mousePos);
-		
-		//Vector3 newVec = transform.position - mousePos;
-		mousePos.z = 0;
-//		transform.LookAt(mousePos);
-		transform.LookAt(mousePos);
-
-
+		//move
+		if (Input.GetKey(KeyCode.W)){
+			rb.AddForce(transform.forward * Time.deltaTime);
+		}
 
 	}
+
+
 }
